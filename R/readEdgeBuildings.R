000aa37a854622dc6f7a77927d3ad2d88386283e
hagento <hagen.tockhorn@pik-potsdam.de>
2024-06-14 14:49:55 +0200

    changed ver number

--- R/readEdgeBuildings.R
+++ R/readEdgeBuildings.R
@@ -9,7 +9,7 @@ readEdgeBuildings <- function(subtype = c("FE", "Floorspace")) {
   subtype <- match.arg(subtype)
 
   # input data version
-  ver <- "1.12"
+  ver <- "1.2"
 
   scenarios <- list(
     SSPs  = paste0("SSP", 1:5),
